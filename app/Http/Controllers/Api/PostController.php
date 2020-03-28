679ed1735af5738567b3f8541407afc22272867d
Ahmed Galal <ahmedgalaldev@gmail.com>
2020-03-28 17:55:19 +0200

    finshed get all api posts

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+use App\User;
+use App\Http\Resources\PostResource;
+
+class PostController extends Controller
+{
+    public function index(){
+       return PostResource::collection(
+        Post::all()
+           
+       );
+    }
+}
